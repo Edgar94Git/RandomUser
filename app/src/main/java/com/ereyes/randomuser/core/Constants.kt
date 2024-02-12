4b4ca958e503294a0848aa631cac893464e726fa
Edgar Reyes <gzzenit@gmail.com>
2024-02-11 18:11:04 -0600

    changes

---
+++ app/src/main/java/com/ereyes/randomuser/core/Constants.kt
@@ -0,0 +1,12 @@
+package com.ereyes.randomuser.core
+
+/****
+ * Project: RandomUser
+ * From: com.ereyes.randomuser.core
+ * Created by Edgar Reyes Gonzalez on 2/11/2024 at 4:47 PM
+ * All rights reserved 2024.
+ ****/
+
+object Constants {
+    const val BASE_URL = "https://randomuser.me/";
+}
--- app/src/main/java/com/ereyes/randomuser/presentation/MainActivity.kt
+++ app/src/main/java/com/ereyes/randomuser/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.ereyes.randomuser
+package com.ereyes.randomuser.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ereyes.randomuser.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

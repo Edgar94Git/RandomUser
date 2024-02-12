3b0dbca5a1590d7f9ccb726b76b7dce9e86609a2
Edgar Reyes <gzzenit@gmail.com>
2024-02-11 18:09:19 -0600

    create RandomUserApp and configuration is maniffest

---
+++ app/src/main/java/com/ereyes/RandomUserApp.kt
@@ -0,0 +1,14 @@
+package com.ereyes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/****
+ * Project: RandomUser
+ * From: com.ereyes
+ * Created by Edgar Reyes Gonzalez on 2/11/2024 at 4:21 PM
+ * All rights reserved 2024.
+ ****/
+
+@HiltAndroidApp
+class RandomUserApp: Application()

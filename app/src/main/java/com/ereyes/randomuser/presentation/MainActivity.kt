26b45c31059c4212e275cc0bd6954017fec2fb1f
Edgar Reyes <gzzenit@gmail.com>
2024-02-11 18:21:06 -0600

    implementation viewModel mainActivity

--- app/src/main/java/com/ereyes/randomuser/presentation/MainActivity.kt
+++ app/src/main/java/com/ereyes/randomuser/presentation/MainActivity.kt
@@ -2,13 +2,17 @@ package com.ereyes.randomuser.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.ereyes.randomuser.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<RandomUserViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel.getRandomUser()
     }
 }
